aa116df2aca0941cc45f574aa5fc093cc4516238
root <root@CTSC00960114701.CTS.COM>
2018-09-03 14:56:03 +0530

    Implement methods of REst requests

--- Section1/app.py
+++ Section1/app.py
@@ -8,7 +8,7 @@
 # Note: This server perspective and the browser perspective is just reverse.
 
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -28,31 +28,57 @@ stores = [
 # POST /store data:{name}
 @app.route("/store", methods=["POST"])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data["name"],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 
 # GET /store/<string:name>
 @app.route("/store/<string:name>") # http://10.154.198.12:6776/store/store_name>
 def get_store(name):
-    pass
+    # Iterate over stores
+    # if the store name matches, return it
+    # if none match, return an error message
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+
+    return jsonify({"message": "Store not found..."})
 
 
 # GET /store
 @app.route("/store")
 def get_stores():
-    pass
+    return jsonify({'stores': stores})
 
 
 # POST /store/<string:name>/item {name:, price:}
 @app.route("/store/<string:name>/item", methods=["POST"])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {
+                "name": request_data['name'],
+                "price": request_data["price"]
+            }
+            store["items"].append(new_item)
+            return jsonify(new_item)
+    return jsonify({"message": "Store not found..."})
 
 
 # GET /store/<string:name>/item
 @app.route("/store/<string:name>/item")
 def get_item_in_store(name):
-    pass
+    for store in stores:
+        if store["name"] == name:
+            return jsonify({"items": store["items"]})
+
+    return jsonify({"message": "Store not found..."})
 
 
 app.run("10.154.198.12", port=6776, debug=True)
