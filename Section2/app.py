2ce8514fcf8e9d55600bfd3342588b0d549c5f15
root <root@CTSC00960114701.CTS.COM>
2018-09-04 12:27:10 +0530

    added store [FlaskRestApp]

--- Section2/app.py
+++ Section2/app.py
@@ -1,5 +1,16 @@
+# Aim: Sending a GET request using Flask Restful and
+#      using POSTMAN to validate the request.
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
+
+
+class Store(Resource):
+    def get(self, name):
+        return {"Store": name}
+
+api.add_resource(Store, '/store/<string:name>')
+
+app.run("10.154.198.12", port=6777, debug=True)
